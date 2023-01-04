65dbea81f6303de0a50c24a42cad9278c9990a20
Sergey Osiyuk <osiyuksv@mail.ru>
2023-01-04 18:02:33 +0300

    Task2_GetNumsMtoN

---
+++ Task2_GetNumsMtoN/Program.cs
@@ -0,0 +1,26 @@
+﻿// Вывести через рекурсию все числа по возрастанию в 
+// промежутке от M до N, которые запрашиваем у пользователя
+
+Console.WriteLine("Введите число M");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int start1 = numM + 1;
+int end1 = numN - 1;
+
+if(numM > numN) 
+  {
+    start1 = numN + 1;
+    end1 = numM - 1;
+  }
+
+string PrintSequenceNumbers(int start, int end)
+{
+  
+  if(start == end) return start.ToString();
+
+  return(start + ", " + PrintSequenceNumbers(start + 1, end));
+}
+
+Console.WriteLine(PrintSequenceNumbers(start1, end1));
