5393c4ad43cc5d8834b617f62cd1119bc1151e60
Sergey Osiyuk <osiyuksv@mail.ru>
2023-01-04 20:16:10 +0300

    Task3_SumDigitNunber

---
+++ Task3_SumDigitNunber/Program.cs
@@ -0,0 +1,19 @@
+﻿// Вывести сумму цифр задаваемого
+// пользователем числа N
+
+Console.WriteLine("Введите число N");
+int numN = Convert.ToInt32(Console.ReadLine());
+int SumDigit = 0;
+
+int SumDigitNunber(int number)
+{
+  if(number / 10 == 0) return (number % 10);
+
+  else
+  {
+    SumDigit = number % 10;
+    return(SumDigit + SumDigitNunber(number / 10));
+  }
+}
+
+Console.WriteLine(SumDigitNunber(numN));
