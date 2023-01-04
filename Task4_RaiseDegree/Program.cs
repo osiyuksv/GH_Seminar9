ff8af7e6bbc7251cb08c4c9e3c4057439111963d
Sergey Osiyuk <osiyuksv@mail.ru>
2023-01-04 20:48:11 +0300

    Task4_RaiseDegree

---
+++ Task4_RaiseDegree/Program.cs
@@ -0,0 +1,15 @@
+﻿// Возвести с помощью рекурсии чмсло A в целую 
+// степень числа B, которые задаёт пользователь
+
+Console.WriteLine("Введите целое число A, для возведения его в степень");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число B, для возведения на него в степень");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int RaiseDegree(int foundation, int degree)
+{
+  if(degree == 0) return (1);
+  return(foundation * RaiseDegree(foundation, degree - 1));
+}
+
+Console.WriteLine(RaiseDegree(numA, numB));
