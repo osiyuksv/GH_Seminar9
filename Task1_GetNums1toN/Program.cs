180cf004380040a41016717c6d08e7ab3311d40a
Sergey Osiyuk <osiyuksv@mail.ru>
2023-01-04 17:25:35 +0300

    Task1_GetNums1toN

---
+++ Task1_GetNums1toN/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывести через рекурсию все числа от 1
+// до N, который запрашиваем у пользователя
+
+Console.WriteLine("Введите число N");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+string PrintSequenceNumbers(int start, int end)
+{
+  if(start == end) return(start.ToString());
+
+  return(start + ", " + PrintSequenceNumbers(start + 1, end));
+}
+
+Console.WriteLine(PrintSequenceNumbers(1, numN));
